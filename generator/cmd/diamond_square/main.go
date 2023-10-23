891d79b7fd8c5fdc443e4403ca32a44766180e68
Max Denisenko <meetmorrowsolonmars@gmail.com>
2023-10-23 22:13:37 +0300

    improve color picker

--- generator/cmd/diamond_square/main.go
+++ generator/cmd/diamond_square/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
-	"image"
-	"image/color"
 	"image/jpeg"
 	"log"
-	"math"
 	"os"
 	"time"
 
+	"github.com/meetmorrowsolonmars/openpgl/generator/internal/domain"
 	"github.com/meetmorrowsolonmars/openpgl/generator/internal/domain/algorithms"
 )
 
@@ -17,50 +15,13 @@ func main() {
 	seed := time.Now().UnixNano()
 	diamondSquare := algorithms.NewDiamondSquare(seed)
 
-	size := 513
+	size := 1025
 	altitudes := diamondSquare.Generate(size)
-
-	min, max := math.MaxFloat64, math.SmallestNonzeroFloat64
-
-	mapImage := image.NewRGBA(image.Rect(0, 0, size, size))
-	for y, row := range altitudes {
-		for x, v := range row {
-			var c color.RGBA
-			switch {
-			case v < 0.2:
-				// sea
-				c = color.RGBA{
-					R: 0x13,
-					G: 0x93,
-					B: 0xf5,
-					A: 0xff,
-				}
-			case v < 0.4:
-				// sand
-				c = color.RGBA{
-					R: 0xf5,
-					G: 0xbc,
-					B: 0x2b,
-					A: 0xff,
-				}
-			case v > 0.4:
-				// forest
-				c = color.RGBA{
-					R: 0x0d,
-					G: 0xa1,
-					B: 0x12,
-					A: 0xff,
-				}
-			}
-
-			mapImage.SetRGBA(x, y, c)
-
-			min = math.Min(min, v)
-			max = math.Max(max, v)
-		}
+	converter := domain.NewMapToImageConverter()
+	mapImage, err := converter.Convert(size, size, altitudes)
+	if err != nil {
+		log.Fatal("can't convert map to image")
 	}
-
-	fmt.Printf("min %f, max %f\n", min, max)
 
 	file, err := os.Create("test.jpeg")
 	if err != nil {
